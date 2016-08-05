2c6d3cc0e7b8b3bb2b228c9010725abb78a7472b
Dmytro_Malkin <Dmytro_Malkin@epam.com>
2016-08-05 19:24:11 +0300

    sidebar finished

--- js/inner.js
+++ js/inner.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $("#hide-sidebar").click(function () {
-        $("#sidebar").animate({
-            width: 'toggle'
-        }, 100);
-    });
+        var sideBar = $('#sidebar');
+        var sideBarWidth = +sideBar.css('width').replace('px', '');
+        var leftPosition = sideBar.css('left').replace('px', '');
+        leftPosition == 0 ? sideBar.animate({'left': '-=' + sideBarWidth + 'px'}) : sideBar.animate({'left': '+=' + sideBarWidth + 'px'})
+    })
 });
