8c2473ee15a0d45bd2bbcddcf494a88f105e15a2
Dmytro_Malkin <Dmytro_Malkin@epam.com>
2016-08-08 18:42:05 +0300

    content block added

--- js/inner.js
+++ js/inner.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
     $("#hide-sidebar").click(function () {
         var sideBar = $('#sidebar');
+        var content = $('.content');
+
         var sideBarWidth = +sideBar.css('width').replace('px', '');
+        var contentWidth = +content.css('width').replace('px', '');
+
         var leftPosition = sideBar.css('left').replace('px', '');
-        leftPosition == 0 ? sideBar.animate({'left': '-=' + sideBarWidth + 'px'}) : sideBar.animate({'left': '+=' + sideBarWidth + 'px'})
+        var leftPadding = content.css('padding-left').replace('px', '');
+
+        leftPosition == 0 ? sideBar.animate({'left': '-=' + sideBarWidth + 'px'}) : sideBar.animate({'left': '+=' + sideBarWidth + 'px'});
+        leftPadding == 260 ? content.animate({'padding-left': '-=' + sideBarWidth + 'px'}) : content.animate({'padding-left': '+=' + sideBarWidth + 'px'});
     })
 });
