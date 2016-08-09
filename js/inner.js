c38753a8f6a72af30d7f27e3db1a8c2d86d75fd5
Dmytro_Malkin <Dmytro_Malkin@epam.com>
2016-08-09 16:00:32 +0300

    go up button added

--- js/inner.js
+++ js/inner.js
@@ -11,5 +11,28 @@ $(document).ready(function () {
 
         leftPosition == 0 ? sideBar.animate({'left': '-=' + sideBarWidth + 'px'}) : sideBar.animate({'left': '+=' + sideBarWidth + 'px'});
         leftPadding == 260 ? content.animate({'padding-left': '-=' + sideBarWidth + 'px'}) : content.animate({'padding-left': '+=' + sideBarWidth + 'px'});
-    })
+    });
+
+
+    if ($('#go-up').length) {
+        var scrollTrigger = 100,
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#go-up').addClass('show');
+                } else {
+                    $('#go-up').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#go-up').on('click', function () {
+            console.log($('#go-up').length);
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
 });
